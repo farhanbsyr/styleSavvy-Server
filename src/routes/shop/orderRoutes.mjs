8565c7f9205753d28a6340ba25fc1f6f1ec4b9d1
unknown <farhanalktr91@gmail.com>
2024-10-20 11:11:35 +0700

    fix routes

--- src/routes/shop/orderRoutes.mjs
+++ src/routes/shop/orderRoutes.mjs
@@ -11,6 +11,6 @@ const router = Router();
 router.post("/create", createOrder);
 router.post("/capture", capturePayment);
 router.get("/list/:userId", getAllOrdersByUser);
-router.get("/detail/:id", getOrderDetails);
+router.get("/details/:id", getOrderDetails);
 
 export default router;
